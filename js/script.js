b76000753387b69adf5209eb5c1ca298bf865295
colletemine <jepngetichcolleta@gmail.com>
2022-02-05 22:56:15 +0300

    cheching on indentation on both html and css file

--- js/script.js
+++ js/script.js
@@ -1,73 +1,63 @@
-function onClick(){
+function onClick() {
   document.getElementById("answerbag").style.display = "none";
   document.getElementById("score-div").style.display = "block";
 }
 
-function myButton(){
-var score = 0;
-var currentAnswer1 = document.answerbag.question1?.value;
-var currentAnswer2 = document.answerbag.question2?.value;
-var currentAnswer3 = document.answerbag.question3?.value;
-var currentAnswer4 = document.answerbag.question4?.value;
-var currentAnswer5 = document.answerbag.question5?.value;
-var currentAnswer6 = document.answerbag.question6?.value;
-var currentAnswer7 = document.answerbag.question7?.value;
-var currentAnswer8 = document.answerbag.question8?.value;
-var currentAnswer9 = document.answerbag.question9?.value;
-var currentAnswer10 = document.answerbag.question10?.value;
+function myButton() {
+  var score = 0;
+  var currentAnswer1 = document.answerbag.question1?.value;
+  var currentAnswer2 = document.answerbag.question2?.value;
+  var currentAnswer3 = document.answerbag.question3?.value;
+  var currentAnswer4 = document.answerbag.question4?.value;
+  var currentAnswer5 = document.answerbag.question5?.value;
+  var currentAnswer6 = document.answerbag.question6?.value;
+  var currentAnswer7 = document.answerbag.question7?.value;
+  var currentAnswer8 = document.answerbag.question8?.value;
+  var currentAnswer9 = document.answerbag.question9?.value;
+  var currentAnswer10 = document.answerbag.question10?.value;
 
-if (currentAnswer1=="substr()"){score+=10};
-if (currentAnswer2=="document.myform.elements.length"){score+=10};
-if (currentAnswer3=="whether there is javascript or not"){score+=10};
-if (currentAnswer4=="display the content of the tags <noscript>"){score+=10};
-if (currentAnswer5=="/* var x = 0; */ var y = 0;"){score+=10};
-if (currentAnswer6=="onmouseout"){score+=10};
-if (currentAnswer7=="onmousescroller"){score+=10};
-if (currentAnswer8=="Both A and B are true."){score+=10};
-if (currentAnswer9=="charAt()"){score+=10};
-if (currentAnswer10=="sum()"){score+=10};
+  if (currentAnswer1 == "substr()") {
+    score += 10
+  };
+  if (currentAnswer2 == "document.myform.elements.length") {
+    score += 10
+  };
+  if (currentAnswer3 == "whether there is javascript or not") {
+    score += 10
+  };
+  if (currentAnswer4 == "display the content of the tags <noscript>") {
+    score += 10
+  };
+  if (currentAnswer5 == "/* var x = 0; */ var y = 0;") {
+    score += 10
+  };
+  if (currentAnswer6 == "onmouseout") {
+    score += 10
+  };
+  if (currentAnswer7 == "onmousescroller") {
+    score += 10
+  };
+  if (currentAnswer8 == "Both A and B are true.") {
+    score += 10
+  };
+  if (currentAnswer9 == "charAt()") {
+    score += 10
+  };
+  if (currentAnswer10 == "sum()") {
+    score += 10
+  };
 
-document.getElementById("score").textContent = score
-if (score > 79) {
-  message.textContent= "You have excellently passed."
+  const messageText = document.querySelector("#message")
 
-} else if (score > 49 && score < 80){
-  message.textContent= " You have fairly passed."
-  
-} else {
-  message.textContent= "you have scored poorly and they need to retake the test."
+  document.getElementById("score").textContent = score
+  if (score > 79) {
+    messageText.textContent = "You have excellently passed."
+
+  } else if (score > 49 && score < 80) {
+    messageText.textContent = " You have fairly passed."
+
+  } else {
+    messageText.textContent = "you have scored poorly and they need to retake the test."
+  }
+  onClick()
 }
-onClick()
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
